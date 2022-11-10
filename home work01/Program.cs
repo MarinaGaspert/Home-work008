f694c0970f89b01fb4a2bc2f950a8e84d14878b1
Marina <gaspertrishka@mail.ru>
2022-11-10 18:22:08 +0700

    задание 1

---
+++ home work01/Program.cs
@@ -0,0 +1,75 @@
+﻿/*Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой
+ строки двумерного массива.
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+В итоге получается вот такой массив:
+7 4 2 1
+9 5 3 2
+8 4 4 2*/
+int n = GetNumber("Введите количество столбцов ");
+int m = GetNumber("Введите количество строк ");
+int[,] matrix = IntitMatrix(m, n);
+
+SortToLower(matrix);
+PrintArray(matrix);
+PrintArray(matrix);
+
+int[,] IntitMatrix(int m, int n)
+{
+    Random rnd = new();
+    int[,] matrix = new int[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            matrix[i, j] = rnd.Next(1, 10);
+        }
+    }
+    return matrix;
+}
+
+int GetNumber(string message)
+{
+    Console.WriteLine(message);
+    bool isCorrect = false;
+    int result = 0;
+    while (!isCorrect)
+        if (int.TryParse(Console.ReadLine(), out result))
+            isCorrect = true;
+        else
+            Console.WriteLine("Введено не число. Повторите ввод.");
+    return result;
+}
+void PrintArray(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+void SortToLower(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int k = 0; k < array.GetLength(1) - 1; k++)
+            {
+                if (array[i, k] < array[i, k + 1])
+                {
+                    int temp = array[i, k + 1];
+                    array[i, k + 1] = array[i, k];
+                    array[i, k] = temp;
+                }
+            }
+        }
+    }
+}
